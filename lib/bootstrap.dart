f256a90cc224863e7a1a380e411c847ff918d1f2
Prototype v1 <106340187+Prototypev1@users.noreply.github.com>
2025-08-11 19:25:57 +0200

    crashlytics

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,9 +2,14 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:logger_service/logger_service.dart';
 import 'package:warranty_saver/di/di.dart';
 import 'package:warranty_saver/core/presentation/navigation/router_configuration.dart';
+import 'package:warranty_saver/firebase_options.dart';
 import 'package:warranty_saver/gen/codegen_loader.g.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
@@ -20,6 +25,18 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
       await EasyLocalization.ensureInitialized();
 
       await initDI();
+
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+
+      if (!kIsWeb) {
+        FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+        Log.onError = (message, _, stackTrace) {
+          FirebaseCrashlytics.instance.recordError(message, stackTrace);
+        };
+      }
+
       getIt.get<RouterConfiguration>().init();
 
       runApp(
@@ -34,8 +51,16 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
         ),
       );
     },
-    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
+    (error, stackTrace) {
+      log(error.toString(), stackTrace: stackTrace);
+      Log().e('Uncaught error in app', error, stackTrace);
+      if (!kIsWeb) {
+        FirebaseCrashlytics.instance.recordError(error, stackTrace);
+      }
+    },
   );
 }
 
-void _initLogger() {}
+void _initLogger() {
+  Log.tag = 'Warranty Saver';
+}
