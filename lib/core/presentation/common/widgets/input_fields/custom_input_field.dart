c3e466bb57b67e9ffab53e1018cb09cb382df6b5
Prototype v1 <106340187+Prototypev1@users.noreply.github.com>
2025-08-15 01:50:25 +0200

    a

--- lib/core/presentation/common/widgets/input_fields/custom_input_field.dart
+++ lib/core/presentation/common/widgets/input_fields/custom_input_field.dart
@@ -1,10 +1,200 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:warranty_saver/core/presentation/common/styles/custom_text_styles.dart';
 
-class CustomInputField extends StatelessWidget {
-  const CustomInputField({super.key});
+class CustomTextField extends StatefulWidget {
+  const CustomTextField({
+    super.key,
+    this.controller,
+    this.initialValue,
+    this.hintText,
+    this.labelText,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.enabled = true,
+    this.autofocus = false,
+    this.obscureText = false,
+    this.onChanged,
+    this.validator,
+    this.onSaved,
+    this.contentPadding = const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+    this.borderRadius = const BorderRadius.all(Radius.circular(8)),
+    this.enabledBorderColor,
+    this.focusedBorderColor,
+    this.errorBorderColor,
+    this.disabledBorderColor,
+    this.hintStyle,
+    this.textStyle,
+    this.labelStyle,
+    this.fillColor,
+    this.filled = false,
+    this.width = double.infinity,
+    this.height = 50,
+    this.textColor,
+    this.cursorColor,
+    this.textInputAction = TextInputAction.done,
+    this.textInputType = TextInputType.text,
+  });
+
+  final TextEditingController? controller;
+  final String? initialValue;
+  final String? hintText;
+  final String? labelText;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final bool enabled;
+  final bool autofocus;
+  final bool obscureText;
+  final ValueChanged<String>? onChanged;
+  final FormFieldValidator<String>? validator;
+  final FormFieldSetter<String>? onSaved;
+  final EdgeInsetsGeometry contentPadding;
+  final BorderRadius borderRadius;
+  final Color? enabledBorderColor;
+  final Color? focusedBorderColor;
+  final Color? errorBorderColor;
+  final Color? disabledBorderColor;
+  final TextStyle? textStyle;
+  final TextStyle? hintStyle;
+  final TextStyle? labelStyle;
+  final Color? fillColor;
+  final bool filled;
+  final double width;
+  final double height;
+  final Color? textColor;
+  final Color? cursorColor;
+  final TextInputAction textInputAction;
+  final TextInputType textInputType;
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> with TickerProviderStateMixin {
+  final _node = FocusNode();
+  final _formKey = GlobalKey<FormState>();
+  final _fieldKey = GlobalKey<FormFieldState<String>>();
+  bool _showError = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _node.addListener(() {
+      if (!_node.hasFocus) {
+        _formKey.currentState?.validate();
+        _showError = _fieldKey.currentState?.errorText != null;
+      } else {
+        _showError = false;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _node.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final theme = Theme.of(context);
+    final inputTextColor = widget.textColor ?? Colors.black;
+    final cursorCol = widget.cursorColor ?? inputTextColor;
+
+    final enabledBorder = OutlineInputBorder(
+      borderRadius: widget.borderRadius,
+      borderSide: BorderSide(color: widget.enabledBorderColor ?? theme.dividerColor, width: 2),
+    );
+    final focusedBorder = OutlineInputBorder(
+      borderRadius: widget.borderRadius,
+      borderSide: BorderSide(color: widget.focusedBorderColor ?? cursorCol, width: 2),
+    );
+    final errorBorder = OutlineInputBorder(
+      borderRadius: widget.borderRadius,
+      borderSide: BorderSide(color: widget.errorBorderColor ?? theme.colorScheme.error, width: 2),
+    );
+    final disabledBorder = OutlineInputBorder(
+      borderRadius: widget.borderRadius,
+      borderSide: BorderSide(color: widget.disabledBorderColor ?? theme.disabledColor, width: 2),
+    );
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (widget.labelText != null)
+          Text(
+            widget.labelText!,
+            style: widget.labelStyle ?? CustomTextStyles.of(context).medium14.apply(color: Colors.black),
+          ),
+        SizedBox(
+          width: widget.width,
+          height: widget.height,
+          child: Form(
+            key: _formKey,
+            child: TextFormField(
+              key: _fieldKey,
+              controller: widget.controller,
+              initialValue: widget.controller == null ? widget.initialValue : null,
+              keyboardType: widget.textInputType,
+              textInputAction: widget.textInputAction,
+              autofocus: widget.autofocus,
+              enabled: widget.enabled,
+              obscureText: widget.obscureText,
+              style: widget.textStyle ?? TextStyle(color: inputTextColor),
+              cursorColor: cursorCol,
+              autovalidateMode: AutovalidateMode.disabled,
+              onChanged: (v) {
+                widget.onChanged?.call(v);
+                _fieldKey.currentState?.validate();
+                if (_node.hasFocus) {
+                  if ((_fieldKey.currentState?.errorText) == null) {
+                    _showError = false;
+                  }
+                } else {
+                  _showError = _fieldKey.currentState?.errorText != null;
+                }
+                setState(() {});
+              },
+              validator: widget.validator,
+              focusNode: _node,
+              onSaved: widget.onSaved,
+              decoration: InputDecoration(
+                hintText: widget.hintText,
+                hintStyle: widget.hintStyle ?? theme.inputDecorationTheme.hintStyle?.copyWith(color: inputTextColor),
+                prefixIcon: widget.prefixIcon,
+                suffixIcon: widget.suffixIcon,
+                contentPadding: widget.contentPadding,
+                enabledBorder: enabledBorder,
+                focusedBorder: focusedBorder,
+                errorBorder: errorBorder,
+                disabledBorder: disabledBorder,
+                border: enabledBorder,
+                filled: widget.filled,
+                fillColor: widget.fillColor,
+                errorStyle: const TextStyle(fontSize: 0, height: 0),
+                errorMaxLines: 1,
+              ),
+            ),
+          ),
+        ),
+        AnimatedSize(
+          duration: const Duration(milliseconds: 180),
+          curve: Curves.easeOut,
+          alignment: Alignment.topLeft,
+          child: Builder(
+            builder: (_) {
+              final error = _fieldKey.currentState?.errorText;
+              if (!_showError || error == null) return const SizedBox.shrink();
+              return Text(
+                error,
+                style: CustomTextStyles.of(context).medium12.apply(color: theme.colorScheme.error),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              );
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/feature/register_page/presentation/register_page.dart
+++ lib/feature/register_page/presentation/register_page.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:warranty_saver/core/presentation/common/styles/custom_text_styles.dart';
+import 'package:warranty_saver/core/presentation/common/utils/helpers/helper.dart';
 import 'package:warranty_saver/core/presentation/common/widgets/buttons/clickable_text.dart';
 import 'package:warranty_saver/core/presentation/common/widgets/buttons/custom_register_button.dart';
+import 'package:warranty_saver/core/presentation/common/widgets/input_fields/custom_input_field.dart';
 import 'package:warranty_saver/di/di.dart';
 import 'package:warranty_saver/feature/register_page/domain/cubit/register_page_cubit.dart';
 import 'package:warranty_saver/feature/register_page/domain/cubit/register_page_state.dart';
@@ -20,6 +22,10 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   late final RegisterPageCubit _registerPageCubit;
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _repeatPasswordController = TextEditingController();
 
   @override
   void initState() {
@@ -28,42 +34,110 @@ class _RegisterPageState extends State<RegisterPage> {
   }
 
   @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _repeatPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: BlocConsumer<RegisterPageCubit, RegisterPageState>(
-        bloc: _registerPageCubit,
-        listener: (context, state) {},
-        builder: (context, state) {
-          return Scaffold(
-            backgroundColor: Colors.white,
-            body: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-              child: Column(
-                children: [
-                  Text(
-                    LocaleKeys.splash_page_warranty_saver.tr(),
-                    style: CustomTextStyles.of(context).bold30.apply(color: Colors.black),
-                  ),
-                  const Spacer(),
-                  CustomRegisterButton(onPressed: () {}, text: 'Register'),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Text(
-                        'Or if you already have an account ',
-                        style: CustomTextStyles.of(context).regular14.apply(color: Colors.black),
-                      ),
-                      ClickableText(
-                        text: 'Login',
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: BlocConsumer<RegisterPageCubit, RegisterPageState>(
+          bloc: _registerPageCubit,
+          listener: (context, state) {},
+          builder: (context, state) {
+            if (state is RegisterPageStateInitial) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                child: Column(
+                  children: [
+                    Text(
+                      LocaleKeys.splash_page_warranty_saver.tr(),
+                      style: CustomTextStyles.of(context).bold30.apply(color: Colors.black),
+                    ),
+                    const Spacer(),
+                    CustomTextField(
+                      controller: _emailController,
+                      labelText: 'Email',
+                      hintText: 'exampleemail@gmail.com',
+                      cursorColor: Colors.black,
+                      focusedBorderColor: Colors.black,
+                      enabledBorderColor: Colors.grey.shade700,
+                      hintStyle: TextStyle(color: Colors.grey.shade700),
+                      prefixIcon: const Icon(Icons.email, color: Colors.black),
+                      validator: Helper.validateEmail,
+                      textInputAction: TextInputAction.next,
+                    ),
+                    CustomTextField(
+                      labelText: 'Password',
+                      hintText: 'example password',
+                      cursorColor: Colors.black,
+                      focusedBorderColor: Colors.black,
+                      enabledBorderColor: Colors.grey.shade700,
+                      prefixIcon: Icon(Icons.password_rounded, color: Colors.black),
+
+                      suffixIcon: Icon(Icons.visibility_off, color: Colors.black),
+                      hintStyle: TextStyle(color: Colors.grey.shade700),
+                      textInputAction: TextInputAction.next,
+                      validator: Helper.validatePassword,
+                    ),
+                    CustomTextField(
+                      labelText: 'Repeat Password',
+                      hintText: 'example password',
+                      cursorColor: Colors.black,
+                      focusedBorderColor: Colors.black,
+                      enabledBorderColor: Colors.grey.shade700,
+                      prefixIcon: Icon(Icons.password_rounded, color: Colors.black),
+
+                      suffixIcon: Icon(Icons.visibility_off, color: Colors.black),
+                      hintStyle: TextStyle(color: Colors.grey.shade700),
+                      textInputAction: TextInputAction.done,
+
+                      validator: Helper.validatePassword,
+                    ),
+                    const Spacer(),
+                    CustomRegisterButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _registerPageCubit.registerUser(
+                            email: _emailController.text,
+                            password: _passwordController.text,
+                          );
+                        }
+                      },
+                      text: 'Register',
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Text(
+                          'Or if you already have an account ',
+                          style: CustomTextStyles.of(context).regular14.apply(color: Colors.black),
+                        ),
+                        ClickableText(
+                          text: 'Login',
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            } else if (state is RegisterPageStateLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is RegisterPageStateSuccess) {
+              return const Center(child: Text('Registration Successful!'));
+            } else if (state is RegisterPageStateError) {
+              return Center(child: Text('Error: ${state.message}'));
+            }
+
+            return const SizedBox.shrink();
+          },
+        ),
       ),
     );
   }
